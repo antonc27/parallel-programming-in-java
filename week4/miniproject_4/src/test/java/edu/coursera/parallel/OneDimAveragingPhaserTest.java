92f8cfdf41ca6953375ce592c7d7589634f0ce36
antonc27 sw <antonc27@mail.ru>
2020-04-18 18:04:31 +0200

    Pass week 4

--- week4/miniproject_4/src/test/java/edu/coursera/parallel/OneDimAveragingPhaserTest.java
+++ week4/miniproject_4/src/test/java/edu/coursera/parallel/OneDimAveragingPhaserTest.java
@@ -12,7 +12,7 @@ public class OneDimAveragingPhaserTest extends TestCase {
     private static int getNCores() {
         String ncoresStr = System.getenv("COURSERA_GRADER_NCORES");
         if (ncoresStr == null) {
-            return Runtime.getRuntime().availableProcessors();
+            return Runtime.getRuntime().availableProcessors() / 4;
         } else {
             return Integer.parseInt(ncoresStr);
         }
